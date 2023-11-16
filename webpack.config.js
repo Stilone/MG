7ae21559bee7a00e107b8411462373e5c57d8764
Dmitry <Dmitry.Astashin@russianpost.ru>
2023-11-16 15:20:57 +0300

    feat: added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,16 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: path.resolve(__dirname, '..', './src/index.tsx'),
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
     module: {
         rules: [
             {
-                test: /\.(ts|js)x?$/,
+                test: /\.(ts|tsx)x?$/,
                 exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                ],
+                use: 'ts-loader',
             },
             {
                 test: /\.css$/,
@@ -32,13 +28,13 @@ module.exports = {
         ],
     },
     output: {
-        path: path.resolve(__dirname, '..', './build'),
+        path: path.resolve(__dirname, './dist'),
         filename: 'bundle.js',
     },
     mode: 'development',
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', './src/index.html'),
+            template: path.resolve(__dirname, 'src', 'index.html'),
         }),
     ]
 }
